af4d2dfa1a4f3fa81d9c8f3e44e3919026db16ef
Marlon Luan <marlonluan@gmail.com>
2023-10-24 09:31:52 -0300

    a

--- dependency.sh
+++ dependency.sh
@@ -3,13 +3,13 @@
 ./mvnw clean
 
 ./mvnw help:effective-pom -Doutput=effective-pom.xml
-./mvnw -f noparent/pom.xml help:effective-pom -Doutput=noparent/effective-pom.xml
-# ./mvnw -f milestone/pom.xml help:effective-pom -Doutput=milestone/effective-pom.xml
+./mvnw -f noparent/pom.xml help:effective-pom -Doutput=effective-pom.xml
+# ./mvnw -f milestone/pom.xml help:effective-pom -Doutput=effective-pom.xml
 
 ./mvnw dependency:tree -DoutputFile=dependency-tree.txt
-./mvnw -f noparent/pom.xml dependency:tree -DoutputFile=noparent/dependency-tree.txt
-# ./mvnw -f milestone/pom.xml dependency:tree -DoutputFile=milestone/dependency-tree.txt
+./mvnw -f noparent/pom.xml dependency:tree -DoutputFile=dependency-tree.txt
+# ./mvnw -f milestone/pom.xml dependency:tree -DoutputFile=dependency-tree.txt
 
 ./mvnw dependency:resolve-plugins -DoutputFile=resolve-plugins.txt
-./mvnw -f noparent/pom.xml dependency:resolve-plugins -DoutputFile=noparent/resolve-plugins.txt
-# ./mvnw -f milestone/pom.xml dependency:resolve-plugins -DoutputFile=milestone/resolve-plugins.txt
+./mvnw -f noparent/pom.xml dependency:resolve-plugins -DoutputFile=resolve-plugins.txt
+# ./mvnw -f milestone/pom.xml dependency:resolve-plugins -DoutputFile=resolve-plugins.txt
